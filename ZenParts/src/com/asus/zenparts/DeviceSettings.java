4e37ccb765664992c7175ce4014cb31fcd1ed9f7
RONIN808 <shubhamshedage808@gmail.com>
2021-12-18 12:13:54 +0530

    sdm660-common: Import ZenParts V2
    *ZenParts: Update some icons to MD5
    *ZenParts: Backlight Dimmer improvement
    *ZenParts: Add Backlight Dimmer QS Tile
    *ZenParts: Add more KCAL profiles
    *ZenParts: Match system theme
    *ZenParts: Reduce default headphone gain value
    
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

---
+++ ZenParts/src/com/asus/zenparts/DeviceSettings.java
@@ -0,0 +1,407 @@
+/*
+ * Copyright (C) 2018 The Asus-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.asus.zenparts;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SELinux;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.SwitchPreference;
+
+import com.asus.zenparts.kcal.KCalSettingsActivity;
+import com.asus.zenparts.ambient.AmbientGesturePreferenceActivity;
+import com.asus.zenparts.preferences.CustomSeekBarPreference;
+import com.asus.zenparts.preferences.SecureSettingListPreference;
+import com.asus.zenparts.preferences.SecureSettingSwitchPreference;
+import com.asus.zenparts.preferences.VibratorStrengthPreference;
+import com.asus.zenparts.speaker.ClearSpeakerActivity;
+
+import com.asus.zenparts.SuShell;
+import com.asus.zenparts.SuTask;
+
+public class DeviceSettings extends PreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    final static String PREF_TORCH_BRIGHTNESS = "torch_brightness";
+    private final static String TORCH_1_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
+            "spmi/spmi-0/spmi0-03/800f000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_0/max_brightness";
+    private final static String TORCH_2_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
+            "spmi/spmi-0/spmi0-03/800f000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_1/max_brightness";
+
+    final static String PREF_HEADPHONE_GAIN = "headphone_gain";
+    private static final String HEADPHONE_GAIN_PATH = "/sys/kernel/sound_control/headphone_gain";
+    final static String PREF_MICROPHONE_GAIN = "microphone_gain";
+    private static final String MICROPHONE_GAIN_PATH = "/sys/kernel/sound_control/mic_gain";
+    final static String PREF_EARPIECE_GAIN = "earpiece_gain";
+    public static final String EARPIECE_GAIN_PATH = "/sys/kernel/sound_control/earpiece_gain";
+    final static String PREF_SPEAKER_GAIN = "speaker_gain";
+    public static final String SPEAKER_GAIN_PATH = "/sys/kernel/sound_control/speaker_gain";
+
+    public static final String KEY_VIBSTRENGTH = "vib_strength";
+    private static final String CATEGORY_DISPLAY = "display";
+    private static final String PREF_DEVICE_KCAL = "device_kcal";
+    
+    public static final String PREF_SPECTRUM = "spectrum";
+    public static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
+
+    public static final String PREF_GPUBOOST = "gpuboost";
+    public static final String GPUBOOST_SYSTEM_PROPERTY = "persist.zenparts.gpu_profile";
+
+    public static final String PREF_CPUBOOST = "cpuboost";
+    public static final String CPUBOOST_SYSTEM_PROPERTY = "persist.zenparts.cpu_profile";
+    
+    public static final String PERF_MSM_THERMAL = "msmthermal";
+    public static final String MSM_THERMAL_PATH = "/sys/module/msm_thermal/parameters/enabled";
+    public static final String PERF_CORE_CONTROL = "corecontrol";
+    public static final String CORE_CONTROL_PATH = "/sys/module/msm_thermal/core_control/enabled";
+    public static final String PERF_VDD_RESTRICTION = "vddrestrict";
+    public static final String VDD_RESTRICTION_PATH = "/sys/module/msm_thermal/vdd_restriction/enabled";
+    public static final String PREF_CPUCORE = "cpucore";
+    public static final String CPUCORE_SYSTEM_PROPERTY = "persist.cpucore.profile";
+
+    public static final String PREF_BACKLIGHT_DIMMER = "backlight_dimmer";
+    public static final String BACKLIGHT_DIMMER_PATH = "/sys/module/mdss_fb/parameters/backlight_dimmer";
+    public static final String PREF_KEY_FPS_INFO = "fps_info";
+
+    public static final String PREF_TCP = "tcpcongestion";
+    public static final String TCP_SYSTEM_PROPERTY = "persist.tcp.profile";
+
+    private static final String PREF_CLEAR_SPEAKER = "clear_speaker_settings";
+
+    private static final String TAG = "ZenParts";
+    private static final String SELINUX_CATEGORY = "selinux";
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
+
+    private CustomSeekBarPreference mTorchBrightness;
+    private VibratorStrengthPreference mVibratorStrength;
+    private Preference mKcal;
+    private Preference mAmbientPref;
+
+    private CustomSeekBarPreference mHeadphoneGain;
+    private CustomSeekBarPreference mMicrophoneGain;
+    private CustomSeekBarPreference mEarpieceGain;
+    private CustomSeekBarPreference mSpeakerGain;
+    
+    private SecureSettingListPreference mSPECTRUM;
+
+    private SecureSettingListPreference mGPUBOOST;
+    private SecureSettingListPreference mCPUBOOST;
+
+    private Preference mClearSpeakerPref;
+    
+    private SecureSettingSwitchPreference mMsmThermal;
+    private SecureSettingSwitchPreference mCoreControl;
+    private SecureSettingSwitchPreference mVddRestrict;
+    private SecureSettingListPreference mCPUCORE;
+
+    private SecureSettingListPreference mTCP;
+
+    private SecureSettingSwitchPreference mBacklightDimmer;
+    private static Context mContext;
+
+    private SwitchPreference mSelinuxMode;
+    private SwitchPreference mSelinuxPersistence;
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences_asus_parts, rootKey);
+
+    //gains
+        mHeadphoneGain = (CustomSeekBarPreference) findPreference(PREF_HEADPHONE_GAIN);
+        mHeadphoneGain.setOnPreferenceChangeListener(this);
+        mMicrophoneGain = (CustomSeekBarPreference) findPreference(PREF_MICROPHONE_GAIN);
+        mMicrophoneGain.setOnPreferenceChangeListener(this);
+        mEarpieceGain = (CustomSeekBarPreference) findPreference(PREF_EARPIECE_GAIN);
+        mEarpieceGain.setOnPreferenceChangeListener(this);
+        mSpeakerGain = (CustomSeekBarPreference) findPreference(PREF_SPEAKER_GAIN);
+        mSpeakerGain.setOnPreferenceChangeListener(this);
+
+    //SElinux toggle
+    Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
+    mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
+    mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
+    mSelinuxMode.setOnPreferenceChangeListener(this);
+
+    mSelinuxPersistence =
+    (SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
+    mSelinuxPersistence.setOnPreferenceChangeListener(this);
+    mSelinuxPersistence.setChecked(getContext()
+    .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE)
+    .contains(PREF_SELINUX_MODE));
+        
+        mContext = this.getContext();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+
+        String device = FileUtils.getStringProp("ro.build.product", "unknown");
+
+        mTorchBrightness = (CustomSeekBarPreference) findPreference(PREF_TORCH_BRIGHTNESS);
+        mTorchBrightness.setEnabled(FileUtils.fileWritable(TORCH_1_BRIGHTNESS_PATH) &&
+                FileUtils.fileWritable(TORCH_2_BRIGHTNESS_PATH));
+        mTorchBrightness.setOnPreferenceChangeListener(this);
+
+        PreferenceCategory displayCategory = (PreferenceCategory) findPreference(CATEGORY_DISPLAY);
+
+        mVibratorStrength = (VibratorStrengthPreference) findPreference(KEY_VIBSTRENGTH);
+        if (mVibratorStrength != null) {
+            mVibratorStrength.setEnabled(VibratorStrengthPreference.isSupported());
+        }
+
+        if (FileUtils.fileWritable(BACKLIGHT_DIMMER_PATH)) {
+            mBacklightDimmer = (SecureSettingSwitchPreference) findPreference(PREF_BACKLIGHT_DIMMER);
+            mBacklightDimmer.setEnabled(BacklightDimmer.isSupported());
+            mBacklightDimmer.setChecked(BacklightDimmer.isCurrentlyEnabled(this.getContext()));
+            mBacklightDimmer.setOnPreferenceChangeListener(new BacklightDimmer(getContext()));
+        } else {
+            getPreferenceScreen().removePreference(findPreference(PREF_BACKLIGHT_DIMMER));
+        }
+        
+        SwitchPreference fpsInfo = (SwitchPreference) findPreference(PREF_KEY_FPS_INFO);
+        fpsInfo.setChecked(prefs.getBoolean(PREF_KEY_FPS_INFO, false));
+        fpsInfo.setOnPreferenceChangeListener(this);
+
+        mKcal = findPreference(PREF_DEVICE_KCAL);
+
+        mKcal.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), KCalSettingsActivity.class);
+            startActivity(intent);
+            return true;
+        });
+            
+        mSPECTRUM = (SecureSettingListPreference) findPreference(PREF_SPECTRUM);
+        mSPECTRUM.setValue(FileUtils.getStringProp(SPECTRUM_SYSTEM_PROPERTY, "0"));
+        mSPECTRUM.setSummary(mSPECTRUM.getEntry());
+        mSPECTRUM.setOnPreferenceChangeListener(this);
+        
+        //clear speaker
+        mClearSpeakerPref = (Preference) findPreference(PREF_CLEAR_SPEAKER);
+        mClearSpeakerPref.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), ClearSpeakerActivity.class);
+            startActivity(intent);
+            return true;
+        });
+        
+        //MSM Thermal control
+        if (FileUtils.fileWritable(MSM_THERMAL_PATH)) {
+            mMsmThermal = (SecureSettingSwitchPreference) findPreference(PERF_MSM_THERMAL);
+            mMsmThermal.setChecked(FileUtils.getFileValueAsBoolean(MSM_THERMAL_PATH, true));
+            mMsmThermal.setOnPreferenceChangeListener(this);
+        } else {
+            getPreferenceScreen().removePreference(findPreference(PERF_MSM_THERMAL));
+        }
+
+        if (FileUtils.fileWritable(CORE_CONTROL_PATH)) {
+            mCoreControl = (SecureSettingSwitchPreference) findPreference(PERF_CORE_CONTROL);
+            mCoreControl.setChecked(FileUtils.getFileValueAsBoolean(CORE_CONTROL_PATH, true));
+            mCoreControl.setOnPreferenceChangeListener(this);
+        } else {
+            getPreferenceScreen().removePreference(findPreference(PERF_CORE_CONTROL));
+        }
+
+        if (FileUtils.fileWritable(VDD_RESTRICTION_PATH)) {
+            mVddRestrict = (SecureSettingSwitchPreference) findPreference(PERF_VDD_RESTRICTION);
+            mVddRestrict.setChecked(FileUtils.getFileValueAsBoolean(VDD_RESTRICTION_PATH, true));
+            mVddRestrict.setOnPreferenceChangeListener(this);
+        } else {
+            getPreferenceScreen().removePreference(findPreference(PERF_VDD_RESTRICTION));
+        }
+
+        mCPUCORE = (SecureSettingListPreference) findPreference(PREF_CPUCORE);
+        mCPUCORE.setValue(FileUtils.getStringProp(CPUCORE_SYSTEM_PROPERTY, "0"));
+        mCPUCORE.setSummary(mCPUCORE.getEntry());
+        mCPUCORE.setOnPreferenceChangeListener(this);
+
+	// TCP
+	mTCP = (SecureSettingListPreference) findPreference(PREF_TCP);
+	mTCP.setValue(FileUtils.getStringProp(TCP_SYSTEM_PROPERTY, "0"));
+	mTCP.setSummary(mTCP.getEntry());
+	mTCP.setOnPreferenceChangeListener(this);
+
+	//Ambient gestures
+	mAmbientPref = findPreference("ambient_display_gestures");
+        mAmbientPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getContext(), AmbientGesturePreferenceActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+    //boosts
+    mGPUBOOST = (SecureSettingListPreference) findPreference(PREF_GPUBOOST);
+        mGPUBOOST.setValue(FileUtils.getStringProp(GPUBOOST_SYSTEM_PROPERTY, "0"));
+        mGPUBOOST.setSummary(mGPUBOOST.getEntry());
+        mGPUBOOST.setOnPreferenceChangeListener(this);
+
+        mCPUBOOST = (SecureSettingListPreference) findPreference(PREF_CPUBOOST);
+        mCPUBOOST.setValue(FileUtils.getStringProp(CPUBOOST_SYSTEM_PROPERTY, "0"));
+        mCPUBOOST.setSummary(mCPUBOOST.getEntry());
+        mCPUBOOST.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        final String key = preference.getKey();
+        switch (key) {                    
+            case PREF_TORCH_BRIGHTNESS:
+                FileUtils.setValue(TORCH_1_BRIGHTNESS_PATH, (int) value);
+                FileUtils.setValue(TORCH_2_BRIGHTNESS_PATH, (int) value);
+                break;
+                
+            case PREF_SPECTRUM:
+                mSPECTRUM.setValue((String) value);
+                mSPECTRUM.setSummary(mSPECTRUM.getEntry());
+                FileUtils.setStringProp(SPECTRUM_SYSTEM_PROPERTY, (String) value);
+                break;
+            
+            case PERF_MSM_THERMAL:
+                FileUtils.setValue(MSM_THERMAL_PATH, (boolean) value);
+                break;
+
+            case PERF_CORE_CONTROL:
+                FileUtils.setValue(CORE_CONTROL_PATH, (boolean) value);
+                break;
+
+            case PERF_VDD_RESTRICTION:
+                FileUtils.setValue(VDD_RESTRICTION_PATH, (boolean) value);
+                break;
+
+            case PREF_CPUCORE:
+                mCPUCORE.setValue((String) value);
+                mCPUCORE.setSummary(mCPUCORE.getEntry());
+                FileUtils.setStringProp(CPUCORE_SYSTEM_PROPERTY, (String) value);
+                break;
+            
+            case PREF_TCP:
+                mTCP.setValue((String) value);
+                mTCP.setSummary(mTCP.getEntry());
+                FileUtils.setStringProp(TCP_SYSTEM_PROPERTY, (String) value);
+                break;
+               
+            case PREF_KEY_FPS_INFO:
+                boolean enabled = (Boolean) value;
+                Intent fpsinfo = new Intent(this.getContext(), FPSInfoService.class);
+                if (enabled) {
+                    this.getContext().startService(fpsinfo);
+                } else {
+                    this.getContext().stopService(fpsinfo);
+                }
+                break;
+            
+            case PREF_HEADPHONE_GAIN:
+                FileUtils.setValue(HEADPHONE_GAIN_PATH, value + " " + value);
+                break;
+
+            case PREF_MICROPHONE_GAIN:
+                FileUtils.setValue(MICROPHONE_GAIN_PATH, (int) value);
+                break;
+            case PREF_EARPIECE_GAIN:
+                FileUtils.setValue(EARPIECE_GAIN_PATH, (int) value);
+                break;
+            case PREF_SPEAKER_GAIN:
+                FileUtils.setValue(SPEAKER_GAIN_PATH, (int) value);
+               break;
+            case PREF_GPUBOOST:
+               mGPUBOOST.setValue((String) value);
+               mGPUBOOST.setSummary(mGPUBOOST.getEntry());
+               FileUtils.setStringProp(GPUBOOST_SYSTEM_PROPERTY, (String) value);
+               break;
+            case PREF_CPUBOOST:
+               mCPUBOOST.setValue((String) value);
+               mCPUBOOST.setSummary(mCPUBOOST.getEntry());
+               FileUtils.setStringProp(CPUBOOST_SYSTEM_PROPERTY, (String) value);
+               break;
+            case PREF_SELINUX_MODE:
+               if (preference == mSelinuxMode) {
+               boolean enable = (Boolean) value;
+               new SwitchSelinuxTask(getActivity()).execute(enable);
+               setSelinuxEnabled(enable, mSelinuxPersistence.isChecked());
+               return true;
+             } else if (preference == mSelinuxPersistence) {
+               setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) value);
+               return true;
+             }break;
+
+
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private boolean isAppNotInstalled(String uri) {
+        PackageManager packageManager = getContext().getPackageManager();
+        try {
+            packageManager.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return false;
+        } catch (PackageManager.NameNotFoundException e) {
+            return true;
+        }
+    }
+    private void setSelinuxEnabled(boolean status, boolean persistent) {
+        SharedPreferences.Editor editor = getContext()
+            .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+        if (persistent) {
+          editor.putBoolean(PREF_SELINUX_MODE, status);
+        } else {
+          editor.remove(PREF_SELINUX_MODE);
+        }
+        editor.apply();
+        mSelinuxMode.setChecked(status);
+      }
+
+      private class SwitchSelinuxTask extends SuTask<Boolean> {
+        public SwitchSelinuxTask(Context context) {
+          super(context);
+        }
+        @Override
+        protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
+          if (params.length != 1) {
+            Log.e(TAG, "SwitchSelinuxTask: invalid params count");
+            return;
+          }
+          if (params[0]) {
+            SuShell.runWithSuCheck("setenforce 1");
+          } else {
+            SuShell.runWithSuCheck("setenforce 0");
+          }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+
+          super.onPostExecute(result);
+          if (!result) {
+            // Did not work, so restore actual value
+            setSelinuxEnabled(SELinux.isSELinuxEnforced(), mSelinuxPersistence.isChecked());
+          }
+        }
+    }
+}
--- sdm660.mk
+++ sdm660.mk
@@ -400,7 +400,11 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.spectrum.rc \
+    init.spectrum.sh \
+    init.parallax.rc \
+    init.parallax.sh
 
 # Seccomp
 PRODUCT_COPY_FILES += \
@@ -504,3 +508,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# ZenParts
+PRODUCT_PACKAGES += \
+    ZenParts
